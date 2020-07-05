fd3797eb5a9589bff0968e7aea7c0d60475b64df
Ivan Gres <ivangrestorres@gmail.com>
2020-07-05 21:40:09 +0200

    add docs

---
+++ docs/precache-manifest.d349e08e601c33438c5c1ff8426a9b05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf14a6c4cfff8fe6b7fd8ca62b9e1ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25061140396f8443aa75",
+    "url": "/static/css/main.f4dcc947.chunk.css"
+  },
+  {
+    "revision": "af929d8804b4bd75c7e0",
+    "url": "/static/js/2.4b088b4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4b088b4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25061140396f8443aa75",
+    "url": "/static/js/main.f4424a85.chunk.js"
+  },
+  {
+    "revision": "167aaa41d4f4532d130f",
+    "url": "/static/js/runtime-main.0a6d1f4b.js"
+  }
+]);
